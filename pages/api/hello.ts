add15c8d97f8efe5c50a5d9f2a651b138e336fcb
diantoniuksc <162093949+diantoniuksc@users.noreply.github.com>
2025-01-29 23:14:17 +0100

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Test API which returns "John Doe" if API call was successful
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
